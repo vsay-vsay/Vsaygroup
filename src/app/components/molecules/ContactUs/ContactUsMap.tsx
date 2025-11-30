58b99119647b4821b04e6846ba3258c0d2925fca
Yesveer <80391289+Yesveer@users.noreply.github.com>
2025-11-30 20:16:17 +0530

    Fix allowFullScreen attribute in ContactUsMap

--- src/app/components/molecules/ContactUs/ContactUsMap.tsx
+++ src/app/components/molecules/ContactUs/ContactUsMap.tsx
@@ -23,7 +23,7 @@ const ContactUsMap = () => {
               width="100%"
               height="100%"
               style={{ border: 0 }}
-              allowFullScreen=""
+              allowFullScreen
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
             ></iframe>
